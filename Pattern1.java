ffd95883e344531079ac5696349043cfa0eb393f
swapnil965 <tambe.swapnil108@gmail.com>
2025-04-07 16:07:41 +0530

    First pattern program

---
+++ Pattern1.java
@@ -0,0 +1,29 @@
+class Pattern1{
+
+	public static void main(String args[]){
+	
+	int row = 4;
+	for (int i = 1;i<=row;i++){
+	
+		for(int j = 1;j<=row;j++){
+		
+		
+			System.out.print("*\t");
+		
+		
+		
+		}
+		System.out.println("");
+	
+	
+	}
+	
+	
+	
+	}
+
+
+
+
+
+}
