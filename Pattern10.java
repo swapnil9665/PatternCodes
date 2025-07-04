a3b76f88ee8a39672004862d1e8b26ea617484a1
swapnil965 <tambe.swapnil108@gmail.com>
2025-07-05 00:06:29 +0530

    start pattern10

---
+++ Pattern10.java
@@ -0,0 +1,51 @@
+class Pattern10{
+
+static int rows = 4;
+
+public static void main(String args[]){
+
+	for(int i = 1;i<=4;i++){
+
+		for(int j = 1; j<=4;j++){
+
+
+			if(j<=rows-i){
+			
+			System.out.print(" ");
+			
+			}else{
+			
+			System.out.print("*" );
+			
+			}
+		
+		
+		
+		
+		
+		
+		}
+		System.out.println(" ");
+	
+	
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+}
